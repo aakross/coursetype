053e54ee0959c3cef632d8142785aef5d46affff
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2025-05-08 16:56:46 -0600

    08/05/25

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,12 +1,48 @@
-export class Pokemon {
-  public id: number;
-  public name: string;
+import axios from "axios";
+// export class Pokemon {
+//   public id: number;
+//   public name: string;
 
-  constructor(id: number, name: string) {
-    this.id = id;
-    this.name = name;
-    console.log("Constructor");
+//   constructor(id: number, name: string) {
+//     this.id = id;
+//     this.name = name;
+//     console.log("Constructor");
+//   }
+// }
+
+// export const charmander = new Pokemon(1,'a');
+
+export class Pokemon {
+  get imageUrl(): string {
+    return `https://pokemon.com/${this.id}.jpg`;
+  }
+
+  constructor(
+    public readonly id: number,
+    public name: string // public imageUrl: string
+  ) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
+
+  async getMoves() {
+    // const moves = 10;
+
+    const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+    console.log(data.moves);
+
+    return data;
   }
 }
 
-export const charmander = new Pokemon(1,'a');
+export const charmander = new Pokemon(1, "Charmander");
+// console.log(charmander);
+// charmander.scream();
+// charmander.speak();
+//console.log(charmander.getMoves());
+charmander.getMoves();
